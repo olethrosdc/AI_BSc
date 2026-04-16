02a74e06b2b02b2e33a91fd98360d15362877ff1
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2026-04-16 10:05:55 +0200

    betting

--- src/MDPs/betting.py
+++ src/MDPs/betting.py
@@ -1,62 +1,39 @@
+# A betting example
+
 import numpy as np
 
-T = 5
-initial_money = 1
-p_win = 0.5 # This is the winning probability (between 0 and 1)
+def get_betting_MDP(initial_money, p_win, T):
 
-if (initial_money > T):
-    n_states = 1 + 2 * T # no more states are possible
-else:
-    n_states =  1 + initial_money + T 
-
-n_actions = 2
-P = np.zeros([n_states, n_states, n_actions])
-R = np.zeros([n_states, n_actions])
-
-
-## The probabilities and rewards
-for s in range(1, n_states-1):
-    P[s+1, s, 1] = p_win
-    P[s-1, s, 1] = 1 - p_win
-    P[0, s, 0] = 1
-    R[s, 0] = s # we only get a reward at the end. If you change
-                # this to s - c, where c > 0, then your policies will
-                # be different. Why is that?
+    # Here we have the starting state, plus all reachable states
+    if (initial_money > T):
+        n_states = 1 + 2 * T # no more states are possible
+    else:
+        n_states =  1 + initial_money + T # you can at best win T, or lose all your money
+    n_actions = 2
+    P = np.zeros([n_states, n_states, n_actions])
+    R = np.zeros([n_states, n_actions])
+    ## The probabilities and rewards
+    for s in range(1, n_states-1):
+        P[s+1, s, 1] = p_win
+        P[s-1, s, 1] = 1 - p_win
+        P[0, s, 0] = 1
+        R[s, 0] = s # we only get a reward at the end. If you change
+                    # this to s - c, where c > 0, then your policies will
+                    # be different. Why is that?
 
 
-P[0, 0, 0] = 1
-P[0, 0, 1] = 1
-P[0, n_states-1, 0] = 1
-P[n_states -1, n_states-1, 1] = 1 # because T = n_states, we can only reach this at the last step.
+    P[0, 0, 0] = 1
+    P[0, 0, 1] = 1
+    P[0, n_states-1, 0] = 1
+    P[n_states -1, n_states-1, 1] = 1 # because T = n_states, we can only reach this at the last step.
+    return DiscreteMDP(n_states, n_actions, P, R)
 
 
-## The backwards induction algorithm
-def BackwardsInduction(P, R, T):
-    n_states = P.shape[0]
-    n_actions = P.shape[2]
 
-    # Initialise V = R for the last time-step.
-    V = np.zeros([n_states, T])
-    for s in range(n_states):
-        V[s, T - 1] = max(R[s, :])
 
-    # Do the recursion
-    policy = np.zeros([n_states, T])
-    for k in range(T-1):
-        t = T - k - 2 # ensures we go from t = T-2, ... to t = 0.
-        Q = np.zeros([n_states, n_actions])
-        for s in range(n_states):
-            for a in range(n_actions):
-                # Start with the reward of the current step
-                Q[s, a] = R[s, a]
-                # Add the expected next-state value
-                for s2 in range(n_states):
-                    Q[s, a] += V[s2, t+1] * P[s2, s, a]
-            # Maximise over actions
-            V[s, t] = np.max(Q[s, :])
-            policy[s,t] = np.argmax(Q[s, :])
-    return V, Q, policy
-
+# Construct an MDP
+mdp = get_betting_MDP(initial_money=1, p_win=0.5, T=5)
+## Run The backwards induction algorithm
 V, Q, policy = BackwardsInduction(P, R, T)
 
 print("Value function")
